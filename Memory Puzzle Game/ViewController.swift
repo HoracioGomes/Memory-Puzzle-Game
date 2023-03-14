150e7f9274d7ceba11a134c2b5a9476a5055a98e
Horacio <horacio.junior@ibm.com>
2023-03-14 17:40:42 -0300

    Adding aligns and constraints

--- Memory Puzzle Game/ViewController.swift
+++ Memory Puzzle Game/ViewController.swift
@@ -9,11 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var gameView: UIView!
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var wonLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func resetAction(_ sender: Any) {
+    }
+    
+    @IBAction func gameModeAction(_ sender: Any) {
+    }
 }
 
