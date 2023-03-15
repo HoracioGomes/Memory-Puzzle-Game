4f519e844c1e4bd92ebc70fbf73ce24a58b6c0d2
Horacio <horacio.junior@ibm.com>
2023-03-15 17:23:39 -0300

    created LabelCard class

---
+++ Memory Puzzle Game/LabelCard.swift
@@ -0,0 +1,16 @@
+//
+//  LabelCard.swift
+//  Memory Puzzle Game
+//
+//  Created by Horacio Gomes dos Santos Junior on 15/03/23.
+//
+
+import Foundation
+import UIKit
+
+class LabelCard : UILabel{
+    
+    var internalNumber: Int!
+    static var question = "?"
+    static var smile = "🙂"
+}
--- Memory Puzzle Game/ViewController.swift
+++ Memory Puzzle Game/ViewController.swift
@@ -32,17 +32,27 @@ class ViewController: UIViewController {
         let tileCGSize = CGSize(width: tileSize-5, height: tileSize-5)
         var xCenter = tileSize / 2.0
         var yCenter = tileSize / 2.0
+        var counter = 1
     
-        for y in 0..<gameMode {
-            for h in 0..<gameMode {
+        for _ in 0..<gameMode {
+            for _ in 0..<gameMode {
                 let tileCenter = CGPoint(x: xCenter, y: yCenter)
-                let tile = UILabel(frame: CGRect(origin: CGPoint.zero, size: tileCGSize))
+                let tile = LabelCard(frame: CGRect(origin: CGPoint.zero, size: tileCGSize))
+                tile.font = UIFont.systemFont(ofSize: 50, weight: UIFont.Weight.bold)
+                tile.textAlignment = NSTextAlignment.center
+                tile.textColor = UIColor.white
+                tile.internalNumber = counter
+                tile.text = "\(tile.internalNumber!)"
+                if(counter == gameMode * gameMode / 2){
+                    counter = 0
+                }
+                counter += 1
                 tile.center = tileCenter
                 xCenter += tileSize
                 tile.backgroundColor = UIColor.black
                 gameView.addSubview(tile)
             }
-            xCenter = tileSize / 2.0
+            xCenter = tileSize / 2
             yCenter += tileSize
         }
         
