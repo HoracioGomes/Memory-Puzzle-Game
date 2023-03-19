981d36d97736acc3a9497491f52ac65a8e0b61de
Horacio <horacio.junior@ibm.com>
2023-03-19 18:27:48 -0300

    Game mode implemented

--- Memory Puzzle Game/ViewController+GameOptions.swift
+++ Memory Puzzle Game/ViewController+GameOptions.swift
@@ -10,5 +10,24 @@ import UIKit
 extension ViewController{
     
     @IBAction func gameModeAction(_ sender: Any) {
+        
+        if let segCtrl =  sender as? UISegmentedControl{
+            
+            let ind = segCtrl.selectedSegmentIndex
+            
+            switch ind {
+            case 0:
+                gameMode = 4
+            case 1:
+                gameMode = 6
+            case 2:
+                gameMode = 8
+            default:
+                break
+            }
+            
+            resetAction(self)
+            
+        }
     }
 }
